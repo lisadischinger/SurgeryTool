cf665b245c0f519b1f3af19e56262d9211870d6a
lisadischinger <45405024+lisadischinger@users.noreply.github.com>
2018-12-30 11:40:59 -0700

    This is getting the MPU IMU up and running
     Only the arduino side works, there is no integration yet, and only a single IMU is hooked up. The Python side is not yet fixed

---
+++ LMD_firmata_addon_MPU.py
@@ -0,0 +1,137 @@
+
+import atexit
+from binascii import hexlify
+import logging
+import math
+import struct
+
+from PyMata.pymata import PyMata
+
+# Constants that define the Circuit Playground Firmata command values.
+
+LMD_COMMAND                 = 0x01                          # Byte that identifies all Circuit Playground commands.
+LMD_IMU_READ                = 0x10                          # Return the current x, y, z accelerometer values.
+LMD_IMU_READ_REPLY          = 0x11                          # Result of an IMU read.  Includes 3 floating point values
+                                                            # (4 bytes each) with omega, theat, zeta angles
+
+LMD_ACCEL_READ              = 0X20                          #RETURN THE CURRENT LINEAR ACCELERATION VALUES, a_x, a_y, a_z
+LMD_ACCEL_READ_REPLY        = 0X21                          #Result of an linear accel read, includes 3 floating points
+
+logger = logging.getLogger(__name__)
+first_accel_read = True
+t_edit = 1                                                  # normalizing factor to correct for the clock silliness
+
+
+class LMD_firmata_addons(PyMata):
+
+    def __init__(self, port_id='COM3', bluetooth=True, verbose=True):           #/dev/ttyACM0
+        # PyMata is an old style class so you can't use super.
+        PyMata.__init__(self, port_id, bluetooth, verbose)
+        # Setup handler for response data.
+        # Note that the data length (1) appears to be unused for these sysex
+        # responses.
+        self._command_handler.command_dispatch.update({LMD_COMMAND: [self._response_handler, 1]})
+        # Setup configured callbacks to null.
+        self._imu_callback = None
+        self._accel_callback = None
+
+    def _parse_firmata_byte(self, data):
+        """Parse a byte value from two 7-bit byte firmata response bytes."""
+        if len(data) != 2:
+            raise ValueError('Expected 2 bytes of firmata repsonse for a byte value!')
+        return (data[0] & 0x7F) | ((data[1] & 0x01) << 7)
+
+    def _parse_firmata_float(self, data):
+        """Parse a 4 byte floating point value from a 7-bit byte firmata response
+        byte array.  Each pair of firmata 7-bit response bytes represents a single
+        byte of float data so there should be 8 firmata response bytes total.
+        """
+
+        if len(data) != 8:
+            raise ValueError('Expected 8 bytes of firmata response for floating point value!')
+        # Convert 2 7-bit bytes in little endian format to 1 8-bit byte for each
+        # of the four floating point bytes.
+        raw_bytes = bytearray(4)
+        for i in range(4):
+            raw_bytes[i] = self._parse_firmata_byte(data[i * 2:i * 2 + 2])
+        # Use struct unpack to convert to floating point value.
+        return struct.unpack('<f', raw_bytes)[0]
+
+    def _parse_firmata_long(self, data):
+        """Parse a 4 byte signed long integer value from a 7-bit byte firmata response
+        byte array.  Each pair of firmata 7-bit response bytes represents a single
+        byte of long data so there should be 8 firmata response bytes total.
+        """
+
+        if len(data) != 8:
+            raise ValueError('Expected 2 bytes of firmata response for long value!')
+        # Convert 2 7-bit bytes in little endian format to 1 8-bit byte for each
+        # of the four floating point bytes.
+        raw_bytes = bytearray(4)
+        for i in range(4):
+            raw_bytes[i] = self._parse_firmata_byte(data[i * 2:i * 2 + 2])
+        # Use struct unpack to convert to floating point value.
+        return struct.unpack('<l', raw_bytes)[0]
+
+    def _response_handler(self, data):
+        """Callback invoked when a circuit playground sysex command is received.
+        """
+        global first_accel_read, t_edit
+
+        logger.debug('LMD response: 0x{0}'.format(hexlify(bytearray(data))))
+        if len(data) < 1:
+            logger.warning('Received response with no data!')
+            return
+        # Check what type of response has been received.
+        command = data[0] & 0x7F
+        if command == LMD_IMU_READ_REPLY:
+            # Parse IMU response.
+            if len(data) < 26:
+                logger.warning('Received IMU response with not enough data!')
+                print(' received the Orinetation information')
+                return
+            omega = self._parse_firmata_float(data[2:10])
+            theta = self._parse_firmata_float(data[10:18])
+            zeta = self._parse_firmata_float(data[18:26])
+            if self._imu_callback is not None:
+                self._imu_callback(omega, theta, zeta)
+
+        elif command == LMD_ACCEL_READ_REPLY:
+            # Parse accelerometer response.
+            if len(data) < 34:
+                print('Received Acceleration response with not enough data!!')
+                print('received the Acceleration response')
+                return
+
+            x = self._parse_firmata_float(data[2:10])
+            y = self._parse_firmata_float(data[10:18])
+            z = self._parse_firmata_float(data[18:26])
+            t = (self._parse_firmata_long(data[26:34]))/1000          # gather time info and translate from millis to s
+            #if first_accel_read:
+                #t_edit = t-1                                          # we know this first value is supposed to be about 1, i know this is bad but I
+                #first_accel_read = False                                #have no idea how to not have an initial time of 1 million
+            #t = t - t_edit
+            if self._accel_callback is not None:
+                self._accel_callback(x, y, z, t)
+        else:
+            logger.warning('Received unexpected response!')
+
+    def read_imu(self, callback):
+        """Request an accelerometer reading.  The result will be returned by
+        calling the provided callback function and passing it 3 parameters:
+            - Omega angle
+            - theta angle
+            - Zeta angle
+            """
+        self._imu_callback = callback
+        self._command_handler.send_sysex(LMD_COMMAND, [LMD_IMU_READ])
+
+    def read_accel(self, callback):
+        """Request an accelerometer reading.  The result will be returned by
+        calling the provided callback function and passing it 3 parameters:
+            - X  angular acceleration
+            - Y angular acceleration
+            - Z angular acceleration
+            """
+        self._accel_callback = callback
+        self._command_handler.send_sysex(LMD_COMMAND, [LMD_ACCEL_READ])
---
+++ MPU9250_lmd.py
@@ -0,0 +1,127 @@
+#from matplotlib import pyplot
+import numpy as np
+
+from LMD_firmata_addons import LMD_firmata_addons
+from PyMata.pymata import PyMata
+import time
+import sys
+
+print("hello")
+board = LMD_firmata_addons('COM3')
+
+# Global Variables
+a_compare = []
+bno_ready = False                               # this is a flag that states when the IMU is out puting data
+#v_compare = []
+
+x_accel = [0, 0]                                # create empty lists so that we can gather information over time;
+y_accel = [0, 0]                                    # initial values have been put in
+z_accel = [0, 0]
+x_vel = [0, 0]
+y_vel = [0, 0]
+z_vel = [0, 0]
+x_pos = [0, 0]
+y_pos = [0, 0]
+z_pos = [0, 0]
+
+x_a0 = 0
+x_a1 = 0
+y_a0 = 0
+y_a1 = 0
+z_a0 = 0
+z_a1 = 0
+
+i = 1                                       # counter used to append to the lists and read from the lists
+                                                # 1 is already accounted for since there are the set initial conditions
+
+
+def imu_data(omega, theta, zeta):
+    print(" Receiving orinetation Data [deg]")
+    #print('Omega = {0}'.format(omega))
+    #print('Theta = {0}'.format(theta))
+    #print('Zeta = {0}'.format(zeta))
+
+
+def accel_data(a_x, a_y, a_z, t):
+    global x_accel, y_accel, z_accel, i, a_compare, bno_ready
+    print(" Receiving Acceleration Data in (m/s^2)")
+    #print('x = {0}'.format(a_x))
+    #print('y = {0}'.format(a_y))
+    #print('z = {0}'.format(a_z))
+    #print('t = {0}'.format(t))
+
+    x_accel = np.vstack([x_accel, [t, a_x]])
+    y_accel = np.vstack([y_accel, [t, a_y]])
+    z_accel = np.vstack([z_accel, [t, a_z]])
+
+    a_compare = [x_accel[i-1, :], x_accel[i, :], y_accel[i-1, :], y_accel[i, :], z_accel[i-1, :], z_accel[i, :]]
+    bno_ready = True                            # if the code made it here, the imu is outputting data
+
+
+def calc_vel():
+    # from the gathered acceleration information, calculate the position of the IMU
+    global x_vel, y_vel, z_vel, i, a_compare
+
+    print('calculating the velocities')
+    x_a0 = a_compare[0]                         # though gross, I would rather have this all be easy to follow
+    x_a1 = a_compare[1]
+    y_a0 = a_compare[2]
+    y_a1 = a_compare[3]
+    z_a0 = a_compare[4]
+    z_a1 = a_compare[5]
+
+    t_i = x_a1[0]                                # time used when logging the calculated values
+    dt = x_a0[0] - x_a1[0]                 # find the change in time between the two data points
+    da_x = x_a1[1] - x_a0[1]               # find the difference between the two acceleration values
+    da_y = y_a1[1] - y_a0[1]
+    da_z = z_a1[1] - z_a0[1]
+
+    # calculate the velocity
+    x_vel = np.vstack([x_vel, [t_i, da_x * dt]])     # calculate the x_velocity
+    y_vel = np.vstack([y_vel, [t_i, da_y * dt]])
+    z_vel = np.vstack([z_vel, [t_i, da_z * dt]])
+
+    v_compare = [x_vel[i - 1, :], x_vel[i, :], y_vel[i - 1, :], y_vel[i, :], z_vel[i - 1, :], z_vel[i, :]]
+    return v_compare
+
+
+def calc_pose(v_compare):
+    # from the gathered acceleration information, calculate the position of the IMU
+    global x_pos, y_pos, z_pos, i
+
+    print('calculating the position')
+    x_v0 = v_compare[0]                         # though gross, I would rather have this all be easy to follow
+    x_v1 = v_compare[1]
+    y_v0 = v_compare[2]
+    y_v1 = v_compare[3]
+    z_v0 = v_compare[4]
+    z_v1 = v_compare[5]
+
+    t_i = x_v1[0]  # time used when logging the calculated values
+    dt = x_v0[0] - x_v1[0]  # find the change in time between the two data points
+    dv_x = x_v1[1] - x_v0[1]  # find the difference between the two acceleration values
+    dv_y = y_v1[1] - y_v0[1]
+    dv_z = z_v1[1] - z_v0[1]
+
+    # calculate the position
+    x_pos = np.vstack([x_pos, [t_i, dv_x * dt]])  # calculate the x_velocity
+    y_pos = np.vstack([y_pos, [t_i, dv_y * dt]])
+    z_pos = np.vstack([z_pos, [t_i, dv_z * dt]])
+
+    print(' the current position is: ')
+    print('x = {0}'.format(x_pos))
+    print('y = {0}'.format(y_pos))
+    print('z = {0}'.format(z_pos))
+
+    i = i+1
+
+
+while True:
+    board.read_imu(imu_data)                                # gather orientation data from the imu
+    board.read_accel(accel_data)                            # gather acceleration and time data from the imu
+    if bno_ready:                                           # make sure the imu has been initialized before calculating
+        vel_compare = calc_vel()                   # integrate the accelerometer info to calculate the velocity
+        calc_pose(vel_compare)                                  # integrate the accelerometer into to find the position
+    time.sleep(1.0)
+
+board.close()
